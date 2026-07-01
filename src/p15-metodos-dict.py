8ab2a421874189f43c6d584fe306db1cb197161b
Romina Vergara Carrera <romina.vergaracarrera@gmail.com>
2026-06-30 20:39:42 -0400

    wip: (dict) metodo pop

--- src/p15-metodos-dict.py
+++ src/p15-metodos-dict.py
@@ -20,3 +20,18 @@ diccionario["llave2"] = 18
 
 print( diccionario )
 
+print()
+print( "Eliminar elemento" )
+print( "-----------------" )
+
+# Metodo pop
+
+diccionario = {"celular": 140000, "notebook": 489990, "tablet": 120000, "cargador": 12400 }
+print( "Diccionario original", diccionario)
+
+# Metodod pop
+# Elimina un elemnto de un dict usando la clave 'key'
+# y devuelve el valor de dicho elemento
+valor_eliminado = diccionario.pop("celular")
+print (" - Eliminado 'celular'", diccionario)
+print(f"Valor de 'celular' = {valor_eliminado}")
