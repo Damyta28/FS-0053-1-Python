1a186d9b75630e5c19dbc280ed05cde9a29b210a
Romina Vergara Carrera <romina.vergaracarrera@gmail.com>
2026-06-30 20:30:40 -0400

    wip: agregar y modificar elementos (dict)

---
+++ src/p15-metodos-dict.py
@@ -0,0 +1,22 @@
+print( "Agregar elementos a un diccionario (dict)" )
+print( "-----------------------------------------" )
+
+# Cuando haceos referencia a la clave de un dict
+# que no existe, lo agrega
+#
+diccionario = { "llave 1": 5 }
+
+print( diccionario )
+
+diccionario[ "llave 2"] = 9
+
+print( diccionario )
+
+print()
+print( "Cambiar calor de una clave de un diccionario (dict)" )
+print( "---------------------------------------------------" )
+
+diccionario["llave2"] = 18
+
+print( diccionario )
+
