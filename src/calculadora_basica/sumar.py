a6877c19c2ffc3829cf2747317b8f69c0e2f0c47
Romina Vergara Carrera <romina.vergaracarrera@gmail.com>
2026-07-02 20:54:51 -0400

    docs: fx suma se agregan tipos

--- src/calculadora_basica/sumar.py
+++ src/calculadora_basica/sumar.py
@@ -1,3 +1,9 @@
-def sumar(x, y):
+# Esta sintaxis de int y float
+# indica que puede recibir estos tipos de datos
+
+def sumar(x: int | float, y: int | float):
+    """
+    fx que suma dos numeros
+    """
     print(f"El resultado de la suma es: {x + y}")
-    return x + y
+    
